136953bc1439c7255c60b58bb0bc0a9c9c0a4923
cuong <dominhcuong1989@gmail.com>
2014-11-28 18:17:37 +0700

    1
    Signed-off-by: cuong <dominhcuong1989@gmail.com>

--- JavaScrip/main.js
+++ JavaScrip/main.js
@@ -1,6 +1,16 @@
 // JavaScript Document
 $(document).ready(function(){
-  $("#FrameG").scroll(function(){
+  /*$("#FrameG").scroll(function(){
    
-  });
+  });*/
+  
+  
+  var myScroll = new iScroll($("#FrameG")[0],
+	{
+		scrollbarClass : "scroll",
+		hideScrollbar : false,
+		checkDOMChange : true,
+		checkDOMChanges : true
+        
+	});
 });
